e03fef5a71402ca96938050289dea0b7926b5bac
Aeisha Hicks <ashicks2017@outlook.com>
2019-06-09 01:18:40 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def person=(person)
+    @person = person
+  end
+
+  def person
+    @person
+  end
+
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+  puts  "The Person is walking"
+  end
+end
